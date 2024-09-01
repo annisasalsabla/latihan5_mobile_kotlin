988e20f2289dc527d630ea1ebfd6a69da7712aca
Annisa Salsabila <nisasalsabila23050505@gmail.com>
2024-09-01 13:24:09 +0700

    starter project

--- app/src/main/java/com/annisa/latihan5mobilekotlin/DiskonActivity.kt
+++ app/src/main/java/com/annisa/latihan5mobilekotlin/DiskonActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,25 +15,43 @@ class DiskonActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_diskon)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
         val etTotalBelanja = findViewById<EditText>(R.id.etTotalBelanja)
-        val btnCalculate = findViewById<Button>(R.id.btnCalculate)
-        val tvDiskon = findViewById<TextView>(R.id.tvDiskon)
+        val btnHitungDiskon = findViewById<Button>(R.id.btnHitungDiskon)
+        val tvHasilDiskon = findViewById<TextView>(R.id.tvHasilDiskon)
 
-        btnCalculate.setOnClickListener {
-            val totalBelanja = etTotalBelanja.text.toString().toDouble()
-            val diskon = when {
-                totalBelanja < 100000 -> 0.0
-                totalBelanja <= 500000 -> 0.10 * totalBelanja
-                totalBelanja <= 1000000 -> 0.20 * totalBelanja
-                else -> 0.30 * totalBelanja
+        btnHitungDiskon.setOnClickListener {
+            val totalBelanjaStr = etTotalBelanja.text.toString()
+
+            if (totalBelanjaStr.isEmpty()) {
+                Toast.makeText(this, "Harap masukkan total belanja", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
             }
 
-            tvDiskon.text = "Diskon yang didapatkan: Rp $diskon"
+            val totalBelanja = totalBelanjaStr.toDouble()
+            val diskon = when {
+                totalBelanja < 100000 -> 0.0
+                totalBelanja in 100000.0..500000.0 -> 0.1
+                totalBelanja in 500000.0..1000000.0 -> 0.2
+                else -> 0.3
+            }
+
+            val jumlahDiskon = totalBelanja * diskon
+            val totalPembayaran = totalBelanja - jumlahDiskon
+
+            val hasil = """
+                Total Belanja: Rp. ${"%,.2f".format(totalBelanja)}
+                Diskon: ${diskon * 100}%
+                Total Pembayaran: Rp. ${"%,.2f".format(totalPembayaran)}
+            """.trimIndent()
+
+            tvHasilDiskon.text = hasil
         }
     }
 }
+
