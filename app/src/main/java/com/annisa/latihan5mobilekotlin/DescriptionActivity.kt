8e42e88fda99835a9c0724cf399363e7ed0a521e
Annisa Salsabila <nisasalsabila23050505@gmail.com>
2024-09-15 16:35:08 +0700

    starter project

--- app/src/main/java/com/annisa/latihan5mobilekotlin/DescriptionActivity.kt
+++ app/src/main/java/com/annisa/latihan5mobilekotlin/DescriptionActivity.kt
@@ -1,28 +1,38 @@
 package com.annisa.latihan5mobilekotlin
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 class DescriptionActivity : AppCompatActivity() {
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_description)
 
         val animalName = intent.getStringExtra("animalName")
         val animalDescription = intent.getStringExtra("animalDescription")
+        val animalImageResId = intent.getIntExtra("animalImageResId", -1)
 
         val titleTextView: TextView = findViewById(R.id.animalTitle)
         val descriptionTextView: TextView = findViewById(R.id.animalDescription)
+        val animalImageView: ImageView = findViewById(R.id.animalImageView)
         val backButton: Button = findViewById(R.id.backButton)
 
         titleTextView.text = animalName
         descriptionTextView.text = animalDescription
 
+        // Set the image if the resource ID is valid
+        if (animalImageResId != -1) {
+            animalImageView.setImageResource(animalImageResId)
+        }
+
         backButton.setOnClickListener {
-            finish() // Kembali ke aktivitas sebelumnya
+            finish()
         }
     }
 }
--- app/src/main/java/com/annisa/latihan5mobilekotlin/ListViewActivity.kt
+++ app/src/main/java/com/annisa/latihan5mobilekotlin/ListViewActivity.kt
@@ -1,5 +1,7 @@
 package com.annisa.latihan5mobilekotlin
 
+
+
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,35 +11,49 @@ import android.widget.ListView
 
 class ListViewActivity : AppCompatActivity() {
 
-        // Daftar hewan dan deskripsinya
-        private val animals = arrayOf("Kucing", "Gajah", "Kupu-kupu", "Beruang", "Kelinci", "Kambing", "Sapi")
-        private val animalDescriptions = mapOf(
-            "Kucing" to "Kucing adalah mamalia karnivora yang termasuk dalam keluarga Felidae.",
-            "Gajah" to "Gajah adalah hewan darat terbesar dengan belalai panjang.",
-            "Kupu-kupu" to "Kupu-kupu adalah serangga yang memiliki sayap berwarna indah.",
-            "Beruang" to "Beruang adalah hewan besar dengan bulu tebal, hidup di berbagai habitat.",
-            "Kelinci" to "Kelinci adalah hewan kecil dengan telinga panjang, hidup di daratan.",
-            "Kambing" to "Kambing adalah hewan ternak yang banyak dipelihara untuk daging dan susunya.",
-            "Sapi" to "Sapi adalah hewan besar yang dipelihara untuk daging, susu, dan tenaga."
-        )
+    // Daftar hewan, deskripsi, dan gambar
+    private val animals = arrayOf("Kucing", "Gajah", "Kupu-kupu", "Beruang", "Kelinci", "Kambing", "Sapi")
+    private val animalDescriptions = mapOf(
+        "Kucing" to "Kucing adalah mamalia karnivora yang termasuk dalam keluarga Felidae.",
+        "Gajah" to "Gajah adalah hewan darat terbesar dengan belalai panjang.",
+        "Kupu-kupu" to "Kupu-kupu adalah serangga yang memiliki sayap berwarna indah.",
+        "Beruang" to "Beruang adalah hewan besar dengan bulu tebal, hidup di berbagai habitat.",
+        "Kelinci" to "Kelinci adalah hewan kecil dengan telinga panjang, hidup di daratan.",
+        "Kambing" to "Kambing adalah hewan ternak yang banyak dipelihara untuk daging dan susunya.",
+        "Sapi" to "Sapi adalah hewan besar yang dipelihara untuk daging, susu, dan tenaga."
+    )
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_list_view)
+    private val animalImages = mapOf(
+        "Kucing" to R.drawable.kucing,
+        "Gajah" to R.drawable.gajah,
+        "Kupu-kupu" to R.drawable.kupu_kupu,
+        "Beruang" to R.drawable.beruang,
+        "Kelinci" to R.drawable.kelinci,
+        "Kambing" to R.drawable.kambing,
+        "Sapi" to R.drawable.sapi
+    )
 
-            val listView: ListView = findViewById(R.id.animalListView)
-            val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, animals)
-            listView.adapter = adapter
 
-            listView.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
-                val selectedAnimal = animals[position]
-                val description = animalDescriptions[selectedAnimal]
 
-                val intent = Intent(this, DescriptionActivity::class.java).apply {
-                    putExtra("animalName", selectedAnimal)
-                    putExtra("animalDescription", description)
-                }
-                startActivity(intent)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_view)
+
+        val listView: ListView = findViewById(R.id.animalListView)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, animals)
+        listView.adapter = adapter
+
+        listView.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
+            val selectedAnimal = animals[position]
+            val description = animalDescriptions[selectedAnimal]
+            val imageResId = animalImages[selectedAnimal]
+
+            val intent = Intent(this, DescriptionActivity::class.java).apply {
+                putExtra("animalName", selectedAnimal)
+                putExtra("animalDescription", description)
+                putExtra("animalImageResId", imageResId)
             }
+            startActivity(intent)
         }
     }
+}
